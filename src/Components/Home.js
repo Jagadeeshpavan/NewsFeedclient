1e09099a2a6bf4325bde3fa3dac79a1cbb7f4be5
Laxmikasi <kasireddylaxmi66040@gmail.com>
2024-02-03 19:06:22 +0530

    A

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -538,7 +538,7 @@ const Home = () => {
                   </FacebookShareButton>
                   
                   <WhatsappShareButton
-                   url={`https://news-feedclient.vercel.app/post/${post._id}`} 
+                   url={`https://news-feedclient.vercel.app`} 
                   >
                    <Link to={`/post/${post._id}`}>
                     <WhatsappIcon size={40} round={true}/>
