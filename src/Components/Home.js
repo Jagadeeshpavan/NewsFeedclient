f9f894cb3abd00728c1ff0f4779f2e56f72f5654
Laxmikasi <kasireddylaxmi66040@gmail.com>
2024-02-03 15:51:58 +0530

    hello

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -538,7 +538,7 @@ const Home = () => {
                   </FacebookShareButton>
                   <Link to={`/post/${post._id}`}>
                   <WhatsappShareButton
-                   
+                   url={`/post/${post._id}`} 
                   >
                     <WhatsappIcon size={40} round={true}/>
                   </WhatsappShareButton>
