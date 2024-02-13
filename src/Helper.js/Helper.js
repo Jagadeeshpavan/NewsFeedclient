0add82ae67cff64be23545afd845d0af858106d6
Laxmikasi <kasireddylaxmi66040@gmail.com>
2024-02-13 18:27:22 +0530

    hi

--- src/Helper.js/Helper.js
+++ src/Helper.js/Helper.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://myserver-1xnr.onrender.com'
+// export const BASE_URL = 'https://myserver-1xnr.onrender.com'
 
-//  export const BASE_URL = `https://localhost:5000`
+ export const BASE_URL = `https://localhost:5000`
